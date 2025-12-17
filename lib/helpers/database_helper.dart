09848dba6c63a716d6a586982d370cbdbb3ef6ed
hdy4567 <131739759+hdy4567@users.noreply.github.com>
2025-12-17 20:02:01 +0900

    Implement DatabaseHelper for notes management

---
+++ lib/helpers/database_helper.dart
@@ -0,0 +1,73 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+import '../models/note.dart';
+
+class DatabaseHelper {
+  static final DatabaseHelper instance = DatabaseHelper._init();
+  static Database? _database;
+
+  DatabaseHelper._init();
+
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+    _database = await _initDB('notes.db');
+    return _database!;
+  }
+
+  Future<Database> _initDB(String filePath) async {
+    final dbPath = await getDatabasesPath();
+    final path = join(dbPath, filePath);
+    return await openDatabase(
+      path,
+      version: 1,
+      onCreate: _createDB,
+    );
+  }
+
+  Future _createDB(Database db, int version) async {
+    const idType = 'INTEGER PRIMARY KEY AUTOINCREMENT';
+    const textType = 'TEXT NOT NULL';
+    const doubleType = 'REAL NOT NULL';
+
+    await db.execute('''
+      CREATE TABLE notes (
+        id $idType,
+        title $textType,
+        body $textType,
+        latitude $doubleType,
+        longitude $doubleType,
+        createdAt $textType
+      )
+    ''');
+  }
+
+  Future<int> createNote(Note note) async {
+    final db = await database;
+    return await db.insert('notes', note.toMap());
+  }
+
+  Future<List<Note>> getAllNotes() async {
+    final db = await database;
+    final List<Map<String, dynamic>> maps = await db.query('notes');
+    return List.generate(maps.length, (i) => Note.fromMap(maps[i]));
+  }
+
+  Future<int> updateNote(Note note) async {
+    final db = await database;
+    return await db.update(
+      'notes',
+      note.toMap(),
+      where: 'id = ?',
+      whereArgs: [note.id],
+    );
+  }
+
+  Future<int> deleteNote(int id) async {
+    final db = await database;
+    return await db.delete(
+      'notes',
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+  }
+}
